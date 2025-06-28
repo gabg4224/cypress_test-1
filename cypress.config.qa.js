32d12282700ccbd304f0a67b7f6497997e909f8c
gabg4224 <gabg4224@gmail.com>
2025-06-28 14:30:10 -0300

    añadiendo mocha

--- cypress.config.qa.js
+++ cypress.config.qa.js
@@ -1,15 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
-  env:{
-user_mail:process.env.CYPRESS_user_mail,
-user_password:process.env.CYPRESS_user_password
+
+  env: {
+    user_mail: process.env.CYPRESS_user_mail,
+    user_password: process.env.CYPRESS_user_password
   },
   e2e: {
-    baseUrl: process.env.baseUrl|| "https://www.saucedemo.com/",
+    baseUrl: process.env.baseUrl || "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+
 });
